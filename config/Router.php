caf2245d606e928faacda84a7b4578a11a80c0b6
marcopolo31 <marc.reg083@gmail.com>
2020-11-11 14:58:47 +0100

    create Router

---
+++ config/Router.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\config;
+
+use Exception;
+
+class Router
+{
+
+    public function run()
+    {
+
+        try
+        {
+            if(isset($_GET['p']))
+            {
+                if($_GET['p'] === 'post')
+                {
+                    require '../templates/single.php';
+                }
+                else
+                {
+                    echo "page inconnue";
+                }
+            }
+            else
+            {
+                require '../templates/home.php';
+            }
+        }
+        catch(Exception $e)
+        {
+            echo 'Erreur';
+        }
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -4,29 +4,8 @@ require '../config/dev.php';
 
 require '../vendor/autoload.php';
 
+$router = new App\config\Router();
+$router->run();
 
-if(isset($_GET['p'])){
-
-    $p = $_GET['p'];
-
-}else{
-    
-    $p = 'home';
-
-}
-
-ob_start();
-
-if($p === 'home'){
-
-    require '../templates/home.php';
-
-} else if($p === 'post'){
-
-    require '../templates/single.php';
-
-}
-
-$content = ob_get_clean();
 
 require '../templates/default.php';
